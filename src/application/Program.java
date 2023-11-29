ba1578d137c9c6f7e46c51598b8f2406a1d34f2a
Giulianna Grego <giuliannagrego@yahoo.com.br>
2023-11-29 16:13:52 -0300

    Att: Using BufferedReader to enhance and automate

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,15 +7,10 @@ import java.io.IOException;
 public class Program {
     public static void main(String[] args) {
 
-        // Processo manual de abertura e fechamento
+        // Processo melhorado utilizando BufferedReader
         String path = "C:\\ProgramData\\Temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -25,18 +20,6 @@ public class Program {
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-
-        } finally {
-            try {
-                if(br != null) {
-                    br.close();
-                }
-                if(fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
