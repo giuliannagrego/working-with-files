4ebade3f441be1a861e78832c66655672ef6076d
Giulianna Grego <giuliannagrego@yahoo.com.br>
2023-12-04 16:10:52 -0300

    Att: manipulating files and folders

--- src/application/write/Program.java
+++ src/application/write/Program.java
@@ -9,8 +9,8 @@ public class Program {
 
         String path = "C:\\ProgramData\\Temp\\out.txt";
 
-        // Gera um arquivo e escreve as linhas da String[] lines
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+        // Gera um arquivo e escreve as linhas da String[] lines, com o append -true- ao invés do arquivo ser destruido e refeito, ele escreve nas linhas seguintes do arquivo anterior
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
             for (String line : lines) {
                 bw.write(line);
                 bw.newLine();
